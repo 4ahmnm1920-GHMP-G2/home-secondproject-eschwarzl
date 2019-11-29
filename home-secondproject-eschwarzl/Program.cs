c146a683bae285f43f6af460fdd3bc41e8ade652
unknown <eva.schwarzl@htl-salzburg.ac.at>
2019-11-29 14:54:52 +0100

    added VisualStudio Project Hello from Home and screenshot of console output

---
+++ home-secondproject-eschwarzl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace home_secondproject_eschwarzl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(value: "Hello From HOME, \n Greedings eschwarzl");
+        }
+    }
+}
